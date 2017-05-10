d6eba9f0b7177da618b981eec523c5212c0ebc69
antoninvroom <antonin.marchard@vroomvroom.fr>
2017-05-11 00:33:19 +0200

    update map, delete leaflet and add mapbox gl js for vector tiles

--- app/assets/javascripts/travel.js
+++ app/assets/javascripts/travel.js
@@ -1,12 +1,13 @@
 function grabTravelData(travel_id) {
     url: '/' + travel_id + '/show.json'
 }
-
+/*
 $(document).ready(function() {
     var featureLayer;
     var geojsonLayer;
     var features;
     var bounds;
+    var polyline;
     var map;
 
     function _initMap() {
@@ -30,6 +31,10 @@ $(document).ready(function() {
                 for(var i = 0; i < data.length; i++) {
                     ar.push(data[i].geometry.coordinates.reverse());
                 }
+                var polyline = L.polyline(ar, {color: '#feb47b'}).addTo(map);
+                // get last record of array
+                var last = ar.length-1;
+                var directions = L.mapbox.directions();
                 map.featureLayer.bindPopup('step n°');
                 console.log(data);
                 map.fitBounds(ar);
@@ -63,4 +68,60 @@ $(document).ready(function() {
     _getModal();
     _initMap();
     _aglgoliaSearch();
+});
+*/
+$(document).ready(function() {
+    var map;
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYW50b3RvIiwiYSI6ImNpdm15YmNwNTAwMDUyb3FwbzlzeWluZHcifQ.r44fcNU5pnX3-mYYM495Fw';
+    
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [-96, 37.8],
+      zoom: 3
+    });
+
+    map.on('click', 'markers', function (e) {
+        map.flyTo({center: e.features[0].geometry.coordinates});
+    });
+
+    map.on('mouseenter', 'markers', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'markers', function () {
+        map.getCanvas().style.cursor = '';
+    });
+
+    $.ajax({
+        dataType: 'json',
+        url: grabTravelData(),
+        success: function(data) {
+            geojson = data;
+            map.addSource("markers", {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": data
+                }
+            });
+            map.addLayer({
+                "id": "markers",
+                "type": "circle",
+                "source": "markers",
+                "paint": {
+                    "circle-radius": 7,
+                    "circle-color": "#ff7e5f"
+                },
+            });
+            var ar = [];
+            for(var i = 0; i < data.length; i++) {
+                ar.push(data[i].geometry.coordinates.reverse());
+            }   
+            map.fitBounds(ar);
+        }, error: function(data) {
+            console.log(data + ' error');
+        }
+    });
 });

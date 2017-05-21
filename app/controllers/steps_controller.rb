e0ea89253b18427bfbc3ce1517bd1083ddda254d
antoninvroom <antonin.marchard@vroomvroom.fr>
2017-05-21 03:19:56 +0200

    show embedded steps on travel

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,14 +1,22 @@
 class StepsController < ApplicationController
+  before_filter :check_travel!
+
+  def check_travel!
+    @travel = Travel.find(params[:travel_id]) rescue nil
+    if !@travel
+        redirect_to root_path, :alert => 'Travel not found'
+    end
+  end
 
   def new
     @step = Step.new
   end
 
   def show
+    @step = @travel.steps.find(params[:id])
   end
 
   def create
-    @travel = Travel.find(params[:travel_id])
     @step = @travel.steps.create!(step_params)
     redirect_to @travel, :notice => 'step created!'
   end

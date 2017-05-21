8b2da5d6fa2fb4009cb0c4e676001c0ac3519a86
antoninvroom <antonin.marchard@vroomvroom.fr>
2017-05-21 22:43:00 +0200

    creation of toolbox v01 working

--- app/controllers/toolboxes_controller.rb
+++ app/controllers/toolboxes_controller.rb
@@ -1,15 +1,16 @@
 class ToolboxesController < ApplicationController
-
   def new
     @toolbox = Toolbox.new
   end
 
+  # create with travel / step id's
   def create
-    @step = Step.find(params[:step_id]) if params[:step_id]
+    @travel = Travel.find(params[:travel_id])
+    @step = Step.where(id: params[:step_id])
     @toolbox = Toolbox.new(toolbox_params)
     if @toolbox.save
       puts 'toolbox successfully saved'
-      redirect_to travel_step_path(@step)
+      redirect_to travel_step_path(@travel, @toolbox.step_id)
     else
       puts 'something went wrong'
       puts @toolbox.errors.full_messages
@@ -19,6 +20,6 @@ class ToolboxesController < ApplicationController
 
   private
   def toolbox_params
-    params.require(:toolbox).permit(:step, :tool_area, :links)
+    params.require(:toolbox).permit(:step_id, :tool_area, :links)
   end
 end
--- app/models/toolbox.rb
+++ app/models/toolbox.rb
@@ -1,14 +1,13 @@
 class Toolbox
   include Mongoid::Document
   include Mongoid::Timestamps
-  include Geocoder::Model::Mongoid
 
   # fields
   field :tool_area, type: String
   field :links, type: Array
-  
+
   # step
-  belongs_to :step
+  belongs_to :step, optional: true
 
 
 end
